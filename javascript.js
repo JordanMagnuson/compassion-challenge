5eb2c62a935cf82350992d56f18cef0f5c21e3ab
magjor <jm.magnuson@gmail.com>
2018-11-26 19:22:40 -0800

    Use Papa Parse library to read csv file.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	var challengeList = loadChallengeList();
+		console.log(challengeList);
+
 	console.log( "ready!" );
  $('.slider').slick({
    slidesToShow: 1,
@@ -27,6 +30,21 @@ $(document).ready(function(){
 
    $('.person-slider').slick('slickGoTo', randPerso);
 	 $('.challenge-slider').slick('slickGoTo', randChallenge);
+
  });
 
 });
+
+function loadChallengeList() {
+		var data;
+		Papa.parse('challenge_list.csv', {
+			header: true,
+			download: true,
+			dynamicTyping: true,
+			complete: function(results) {
+				console.log(results);
+				data = results.data;
+			}
+		});
+		return data;
+}
