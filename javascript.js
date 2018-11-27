00646069e49df941630723bac7f815d1c259d88b
magjor <jm.magnuson@gmail.com>
2018-11-26 22:02:21 -0800

    A couple of fixes/tweaks.

--- javascript.js
+++ javascript.js
@@ -2,42 +2,15 @@ $(document).ready(function(){
 	// Create the sliders.
 	createPersonSlider();
 	createChallengeSlider();
- 
+
 	// Randomizer button.
 	$('#spinner a').click(function(e) {
 		e.preventDefault();
-		randomizeSliders();
+		randomizeSlider('#person-slider');
+    randomizeSlider('#challenge-slider');
  });
 
 });
-
-function createChallengeSlider() {
-  var data;
-	// Use Papa Parse library to read csv file.
-	Papa.parse('challenge_list.csv', {
-    header: true,
-		download: true,
-		dynamicTyping: true,
-		complete: function(results) {
-			// Insert html.
-			data = results.data;
-			data.forEach(function(element) {
-				console.log(element);
-				$('#challenge-slider').append('<div><h3>' + element.Title + '</h3></div>');
-				
-			});
-
-			// Create the actual slider now using the Slick Slider library.
-			$('#challenge-slider').slick({
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				arrows: true,
-				dots: true,
-				focusOnSelect: true,
-			});
-		}
-	});
-}
 
 function createPersonSlider() {
 	var data;
@@ -51,42 +24,72 @@ function createPersonSlider() {
 			data = results.data;
 			data.forEach(function(element) {
 				console.log(element);
-				$('#person-slider').append('<div><h3>' + element.Title + '</h3></div>');
-				
+        if (element.Title != null)
+        {
+          $('#person-slider').append('<div><h3>' + element.Title + '</h3><p>' + element.Description + '</p></div>');
+        }
 			});
 
 			// Create the actual slider now using the Slick Slider library.
 			$('#person-slider').slick({
 				slidesToShow: 1,
 				slidesToScroll: 1,
-				arrows: true,
-				dots: true,
+				arrows: false,
+				dots: false,
 				focusOnSelect: true,
 			});
+
+      // Randomize to begin.
+      randomizeSlider('#person-slider');
 		}
 	});
 }
 
-function randomizeSliders() {
+function createChallengeSlider() {
+  var data;
+	// Use Papa Parse library to read csv file.
+	Papa.parse('challenge_list.csv', {
+    header: true,
+		download: true,
+		dynamicTyping: true,
+		complete: function(results) {
+			// Insert html.
+			data = results.data;
+			data.forEach(function(element) {
+				console.log(element);
+        if (element.Title != null)
+        {
+          $('#challenge-slider').append('<div><h3>' + element.Title + '</h3><p>' + element.Description + '</p></div>');
+        }
+			});
+
+			// Create the actual slider now using the Slick Slider library.
+			$('#challenge-slider').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				dots: false,
+				focusOnSelect: true,
+			});
+
+      // Randomize to begin.
+      randomizeSlider('#challenge-slider');
+		}
+	});
+}
+
+function randomizeSlider(selector) {
 	// Get slide counts and current slide.
-	var personCount = $("#person-slider").slick("getSlick").slideCount;
-	var currentPerson = $("#person-slider").slick("getSlick").currentSlide;
-	var challengeCount = $("#challenge-slider").slick("getSlick").slideCount;
-	var currentChallenge = $("#challenge-slider").slick("getSlick").currentSlide;
+	var slideCount = $(selector).slick("getSlick").slideCount;
+	var currentSlide = $(selector).slick("getSlick").currentSlide;
 
 	// Prevent randomizing to current selection.
 	do
 	{
-		var randPerso = Math.floor(Math.random() * personCount);
+		var randomSlide = Math.floor(Math.random() * slideCount);
 	}
-	while (randPerso == currentPerson);
-	do
-	{
-		var randChallenge = Math.floor(Math.random() * challengeCount);
-	}
-	while (randChallenge == currentChallenge);
+	while (randomSlide == currentSlide);
 
 	// Now go to the new slides.
-	$('#person-slider').slick('slickGoTo', randPerso);
-	$('#challenge-slider').slick('slickGoTo', randChallenge);
+	$(selector).slick('slickGoTo', randomSlide);
 }
